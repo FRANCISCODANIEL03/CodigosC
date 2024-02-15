e5c95486fd8c006d4f1964bcb999e507c0ca2b56
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-02-14 18:14:58 -0600

    first commit

---
+++ nombre.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("¡Binvenido Francisco es un gusto compilar para Francisco");
+    return 0;
+}
---
+++ printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    printf("Este es un texto \n");
+    printf("que fue impreso");
+    printf("\n");
+    printf("Ya soy todo un programador");
+    printf("\n");
+    return 0;
+}
