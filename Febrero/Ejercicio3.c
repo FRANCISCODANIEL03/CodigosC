8c660aad6d96ae3c35689110f9cb1344cdc1bc58
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-02-19 11:18:10 -0600

    update

--- Febrero/Ejercicio3.c
+++ Febrero/Ejercicio3.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #define PI 3.1415926
-
+/*Codigo que obtiene el area de un circulo pidiendo el diametro*/
 int main(){
     double Area,Radio;
     int Diam;
-    Diam = 10;
+    puts("Ingresa el diametro del circulo");
+    scanf("%d", &Diam);
     Radio = Diam/2;
     Area = PI*(Radio*Radio);
 
-    printf("El Area del circulo con diametro de %d metros es = %f",Diam,Area);
+    printf("El Area del circulo con diametro de %d metros es = %.3f",Diam,Area);
     return 0;
 }
