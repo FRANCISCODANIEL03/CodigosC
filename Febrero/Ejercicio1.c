65520805eee4283e8b30b7d286a72afa44e348de
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-02-26 21:37:12 -0600

    update

--- Febrero/Ejercicio1.c
+++ Febrero/Ejercicio1.c
@@ -1,11 +1,14 @@
 # include <stdio.h>
-/*Codigo que multiplica tres numeros enteros y da el resultado*/
+/*Codigo que pide tres numeros enteros los multiplica y da el resultado*/
 int main(){
     int V1,V2,V3;
-    V1 = 5;
-    V2 = 8;
-    V3 = 12;
+    printf("Ingresa el primer numero\n");
+    scanf("%d", &V1);
+    printf("Ingresa el segundo  numero\n");
+    scanf("%d", &V2); 
+    printf("Ingresa el tercer numero\n");
+    scanf("%d", &V3);
 
-    printf("El valor de multiplicar %d x %d x %d es = %d",V1,V2,V3,V1*V2*V3);
+    printf("La multiplocacion de  %d x %d x %d es = %d",V1,V2,V3,V1*V2*V3);
     return 0;   
 }    
