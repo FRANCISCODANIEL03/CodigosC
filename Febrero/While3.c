c26d2836441d8c42109c0b1897f52d00edb88eb0
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-02-29 14:47:41 -0600

    Update

--- Febrero/While3.c
+++ Febrero/While3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int num = 1,num2;
-    while(num < 1000){
-        num2 =num %3;
-        if(num2 == 0){
+    int num = 1,num2,num3;
+    puts("Ingresa el limite del rango: 0 a : ");
+    scanf("%d", &num2);
+    puts("Ingresa el numero del que van a aparecer sus multiplos");
+    scanf("%d", &num3);
+    while(num < num2){
+        if(num %num3 == 0){
             printf("%d, ",num);
         }
         num++;
