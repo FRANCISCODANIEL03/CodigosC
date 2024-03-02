336b0a35d217205059a8698087f870c58b5bd693
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-01 21:13:07 -0600

    Update

--- Febrero/While4.c
+++ Febrero/While4.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    int num = 1,num2,num3;
-    while(num < 1000){
-        num2 =num %2;
-        num3 = num %7;
-        if(num2 == 0 && num3 ==0){
+    int num = 1,num2,num3,num4;
+     puts("Ingresa el limite del rango: 0 a : ");
+    scanf("%d", &num2);
+    puts("Ingresa el primer numero del que no van a aparecer sus multiplos");
+    scanf("%d", &num3);
+    puts("Ingresa el segundo numero del que no van a aparecer sus multiplos");
+    scanf("%d", &num4);
+    while(num <= num2){
+        if(num %num3 != 0 && num %num4 !=0){
             printf("%d, ",num);
         }
         num++;
