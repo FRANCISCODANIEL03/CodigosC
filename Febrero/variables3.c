6536d2c23f11e0f6f4ac4db94680b5a7b5f7ef00
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-01 21:11:24 -0600

    Update

--- Febrero/variables3.c
+++ Febrero/variables3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /*Codigo que suma dos numeros flotantes e imprime el resultado*/
 int main(){
-    double a = 3.1;
-    double A = 4.5;
-
-    printf("%f", a + A);
+    float a ;
+    float A ;
+    puts("Ingresa un numero con decimales");
+    scanf("%f", &a);
+    puts("Ingresa un otro numero con decimales");
+    scanf("%f", &A);
+    printf("La suma de estos numeros es: %f\n", a + A);
 
     printf("\n");
     return 0;
