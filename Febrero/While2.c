514af57435ef708089a01e42e62f094452886737
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-01 21:12:12 -0600

    Update

--- Febrero/While2.c
+++ Febrero/While2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int num = 1;
-    while(num <= 1000){
+    int num = 1,num2,num3;
+      puts("Ingresa el limite del rango: 0 a : ");
+    scanf("%d", &num2);
+    while(num <= num2){
         printf("%d, ",num);
         num++;
     }
-    return 0;
+   return 0;
 }
