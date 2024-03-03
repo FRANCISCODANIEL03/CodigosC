0bdf7901672ee0bc5657c972334136e9b9b4dcd0
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-02 21:43:37 -0600

    Update

--- Febrero/Segundos.c
+++ Febrero/Segundos.c
@@ -2,19 +2,9 @@
 /*Codigo que calcula cuantos segundos haz vivido*/
 int main(){
     int seg,min,hora,dia,año;
-   /* printf("Ingresa los minutos que has vivido");
-    scanf("%d",&min);
-    printf("Ingresa las horas que has vivido");
-    scanf("%d",&hora);
-    printf("Ingresa los dias que has vivido");
-    scanf("%d",&dia);
-    printf("Ingresa los años que has vivido");
-    scanf("%d",año);
-    seg= año * dia * hora *min;
-    printf("segundos que has vivido = "+ seg);
-    return 0;*/
+    puts("Ingresa los años que tienes");
     scanf("%d",&año);
     seg =año*365*24*60*60;
-    printf("%d",seg);
+    printf("Los segundos que has vivido son: %d",seg);
     return 0;
 }
