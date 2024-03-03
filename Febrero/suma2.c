0cf150bc33340404c890da38037a7f700c7b9695
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-02 21:42:33 -0600

    Update

--- Febrero/suma2.c
+++ Febrero/suma2.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /*Codigo que suma dos numeros*/
 int main (){
-    int unNumero = 3;
-    int otroNumero = 4;
-
+    int unNumero;
+    int otroNumero;
+    puts("Ingresa un numero ");
+    scanf("%d", &unNumero);
+    puts("Ingresa otro numero ");
+    scanf("%d", &otroNumero);
     //Quiero imprimir la suma de 3 mas 4
-    printf("%d",unNumero+otroNumero);
-    
+    printf("La suma de %d + %d es: %d\n",unNumero,otroNumero,unNumero+otroNumero);
     printf("\n");
     return 0;
 }
