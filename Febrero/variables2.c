4901219763525f865eebbbab2b06a50912859845
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-02 21:41:03 -0600

    Update

--- Febrero/variables2.c
+++ Febrero/variables2.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /*Codigo que calcula el area de un circulo*/
 int main(){
-    double r = 5;
+    double r;
     double pi = 3.1416;
+    puts("Ingresa el radio del circulo");
+    scanf("%lf", &r);
     double area = pi * r * r;
 
-    printf("%f",area);
+    printf("El area del circulo es: %.4lf\n",area);
 
     printf("\n");
     return 0;
