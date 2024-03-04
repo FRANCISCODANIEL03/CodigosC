07da046be9cce1a498aa1d67ba765aa6407d2305
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-03 21:48:17 -0600

    Update

--- Febrero/Ejercicio2.c
+++ Febrero/Ejercicio2.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
 /*Codigo que divide dos numeros con decimales*/
 double main(){
-    double V1,V2;
-    V1 = 5.25;
-    V2 = 7.23;
-
+    float V1,V2;
+    puts("Ingresa un numero con decimales");
+    scanf("%f", &V1);
+    puts("Ingresa un numero con decimales");
+    scanf("%f", &V2);
     printf("La division de %.2f entre %.2f es = %f",V1,V2,V1/V2);
     return 0;
 }
