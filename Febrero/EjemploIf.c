d3c8e3950c59aff80583da2ba5e2281641d1d5e1
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-03 21:50:19 -0600

    Update

--- Febrero/EjemploIf.c
+++ Febrero/EjemploIf.c
@@ -2,9 +2,12 @@
 /*Codigo que pide un numero e identifica si es par a no*/
 int main(){
     int numero ;
+    puts("Ingresa un numero");
     scanf("%d", &numero);
     if (numero %2 == 0){
         printf("El numero es par \n");
+    }else{
+        printf("El numero no es par \n");
     }
     puts("Fin del programa ");
     return 0;
