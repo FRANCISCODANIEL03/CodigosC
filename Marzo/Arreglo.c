ab4eb911330a24052770265a8631edd432c95282
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-06 22:03:36 -0600

    Update

--- Marzo/Arreglo.c
+++ Marzo/Arreglo.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
+/*Codigo que crea un arreglo y guarda un valor en cada una de sus posiciones*/
 int main(){
-   /* int miArreglo[8];
-    miArreglo[0] = 5;
-    miArreglo[1] = 10;
-    miArreglo[2] = miArreglo[0] + miArreglo[1];*/
     int vector[10];
     int i = 0;
     while(i < 10){
          vector[i] = 10;
           printf("vector[%d] = %d\n",i,vector[i]);
          i++;
+
     }
     
     return 0;
