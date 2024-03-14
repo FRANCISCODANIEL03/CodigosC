84fc23d0283d9aaa64a221ad1f5c6a75c190f872
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-13 19:20:10 -0600

    Update

---
+++ Marzo/Arreglo5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    puts("Ingresa el tamaño del vector");
+    scanf("%d", &a);
+    float vec[a];
+    int i = 0;
+    double mult = 1;
+    double sum = 0;
+    if(a == 0){
+        puts("El valor del vector es igual a cero no es posible llenarlo");
+    }
+    else{
+        while(i < a){
+        printf("Ingresa el valor %d \n",i + 1);
+        scanf("%f", &vec[i]);
+        mult = vec[i] * mult;
+        sum = vec[i] + sum;
+        i++;
+        }
+    printf("La multiplicacion de todos los elementos es = %.3f\n",mult);
+    printf("El promedio de todos los elemntos es = %.3f\n",sum / a);
+    }
+    return 0;
+}
