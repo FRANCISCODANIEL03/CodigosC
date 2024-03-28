31d7e3e0bb6ce2227583756fb66ebceeac440706
FRANCISCODANIEL03 <gregorioguevfranciscodan@gmail.com>
2024-03-27 21:38:09 -0600

    Update

--- Marzo/for_par2.c
+++ Marzo/for_par2.c
@@ -5,7 +5,7 @@ int main(){
     int i;
     printf("Prueba: funcion entero elevado a potencia\n");
     for (i = 0; i < 10; i++){
-        printf("2^%d es: %d   ",i,f(2,i));
+        printf("2^%d es: %d\t",i,f(2,i));
         printf("3^%d es: %d\n",i,f(3,i));
     }
     return 0;
